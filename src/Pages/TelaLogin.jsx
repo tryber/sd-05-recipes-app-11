cfae15b7be6f4f04ab4d1099df6cad060b147c38
ferrari <luisdiasferrari@gmail.com>
2020-09-09 01:00:24 -0300

    HTML Login

--- src/Pages/TelaLogin.jsx
+++ src/Pages/TelaLogin.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 export default function Login() {
   return (
@@ -8,6 +8,5 @@ export default function Login() {
       <input data-testid="password-input" type="password" minLength="6">Senha</input>
       <button data-testid="login-submit-btn" type="button" disabled>Entrar</button>
     </div>
-  )
+  );
 }
-
