7aefd6ed9655b181ff85806905494bc76c59cd27
ferrari <luisdiasferrari@gmail.com>
2020-09-09 02:05:37 -0300

    HTML Footer

--- src/Components/MenuInferior.jsx
+++ src/Components/MenuInferior.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from 'react';
 
 export default function MenuInferior() {
   return (
     <footer data-testid="footer">
-      <button data-testid="food-bottom-btn"><img src="src/images/mealIcon" /></button>
-      <button data-testid="explore-bottom-btn"><img src="src/images/exploreIcon" /></button>
-      <button data-testid="drinks-bottom-btn"><img src="src/images/drinkIcon" /></button>
+      <button
+        data-testid="food-bottom-btn" type="button"
+      >
+        <img src="src/images/mealIcon"alt="food" />
+      </button>
+      <button
+        data-testid="explore-bottom-btn" type="button"
+      >
+        <img src="src/images/exploreIcon" alt="explore" />
+      </button>
+      <button
+        data-testid="drinks-bottom-btn" type="button"
+      >
+        <img src="src/images/drinkIcon" alt="drinks" />
+      </button>
     </footer>
-  )
+  );
 }
