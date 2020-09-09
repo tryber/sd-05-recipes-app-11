abe8309e65075c8175417bf3b5081564c9cf1f6e
pauloricardoz <trybe.przt@gmail.com>
2020-09-09 17:15:24 -0300

    header - top e options to click

--- src/Pages/BarraBuscaHeader.jsx
+++ src/Pages/BarraBuscaHeader.jsx
@@ -1,1 +1,13 @@
-.
+import React from 'react';
+import userLogo from '../images/profileIcon.svg';
+import searchLogo from '../images/searchIcon.svg';
+
+export default function BarraBuscaHeader(props) {
+  return (
+    <div className="Bar-Busca-Header">
+      <img src={userLogo} alt="Imagem User" className="Bar-Busca-Header"/>
+      <h1 className="Bar-Busca-Header">Comidas</h1>
+      <img src={searchLogo} alt="Search Logo" className="Bar-Busca-Header" />
+    </div>
+  );
+}
