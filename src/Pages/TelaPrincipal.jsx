3937ab4b9f411551393a271c5b0a17edbe92aa3c
ferrari <luisdiasferrari@gmail.com>
2020-09-09 17:06:00 -0300

    Rotas App, CodeClimate

--- src/Pages/TelaPrincipal.jsx
+++ src/Pages/TelaPrincipal.jsx
@@ -21,5 +21,5 @@ export default function TelaPrincipal(props) {
 }
 
 TelaPrincipal.propTypes = {
-  match: PropTypes.objectOf(PropTypes.object()).isRequired,
+  match: PropTypes.objectOf(PropTypes.object).isRequired,
 };
