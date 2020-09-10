cd966fa5bd561291fb862a0f5e2f44988a0b65cf
luca <luucadecastro@gmail.com>
2020-09-10 17:58:55 -0300

    APIs de bebida e comida

--- src/Services/ApiBebida.js
+++ src/Services/ApiBebida.js
@@ -1,9 +1,27 @@
-import React from 'react';
-
-export default function ApiBebida() {
-  return (
-    <div>
-      .
-    </div>
+export const searchCockTailByName = (name) => {
+  fetchApi(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`).then((response) =>
+    response
+      .json()
+      .then((drink) => drink.drinks)
+      .catch((error) => error)
   );
-}
+};
+
+export const searchCockTailByLetter = (letter) => {
+  fetchApi(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${letter}`).then((response) =>
+    response
+      .json()
+      .then((drink) => drink.drinks)
+      .catch((error) => error)
+  );
+};
+
+export const searchCockTailByIngredient = (mainIngredient) => {
+  fetchApi(`https://www.thecocktaildb.com/api/json/v1/1/search.php?i=${mainIngredient}`).then(
+    (response) =>
+      response
+        .json()
+        .then((drink) => drink.drinks)
+        .catch((error) => error)
+  );
+};
