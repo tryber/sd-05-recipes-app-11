a69dea9d16920293195da055205ba81d642e0258
pauloricardoz <trybe.przt@gmail.com>
2020-09-10 19:48:13 -0300

    login localStorage and Link to Comidas done

--- src/Pages/TelaLogin.jsx
+++ src/Pages/TelaLogin.jsx
@@ -1,20 +1,48 @@
 import React, { useState } from 'react';
 import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 // HA https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
 function validateEmail(email) {
-  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
 }
 
-function onclickFuntion() {
-  localStorage.setItem('mealsToken', 'tokenAqui');
-  localStorage.setItem('cocktailsToken', 'tokenAqui');
-  localStorage.setItem('user', 'tokenAqui');
-/*   localStorage.setItem('doneRecipes', 'tokenAqui');
-  localStorage.setItem('favoriteRecipes ', 'tokenAqui');
-  localStorage.setItem('inProgressRecipes', 'tokenAqui'); */
+function onclickFuntion(email) {
+  if (!localStorage.getItem('mealsToken')) localStorage.setItem('mealsToken', '1');
+  if (!localStorage.getItem('cocktailsToken')) localStorage.setItem('cocktailsToken', '1');
+  if (!localStorage.getItem('user')) localStorage.setItem('user', JSON.stringify({ email }));
 }
+/*   localStorage.setItem('doneRecipes', [{
+      id: id-da-receita,
+      type: comida-ou-bebida,
+      area: area-da-receita-ou-texto-vazio,
+      category: categoria-da-receita-ou-texto-vazio,
+      alcoholicOrNot: alcoholic-ou-non-alcoholic-ou-texto-vazio,
+      name: nome-da-receita,
+      image: imagem-da-receita,
+      doneDate: quando-a-receita-foi-concluida,
+      tags: array-de-tags-da-receita-ou-array-vazio
+  }]);
+    localStorage.setItem('favoriteRecipes ', [{
+      id: id-da-receita,
+      type: comida-ou-bebida,
+      area: area-da-receita-ou-texto-vazio,
+      category: categoria-da-receita-ou-texto-vazio,
+      alcoholicOrNot: alcoholic-ou-non-alcoholic-ou-texto-vazio,
+      name: nome-da-receita,
+      image: imagem-da-receita
+  }]);
+    localStorage.setItem('inProgressRecipes', {
+      cocktails: {
+          id-da-bebida: [lista-de-ingredientes-utilizados],
+          ...
+      },
+      meals: {
+          id-da-comida: [lista-de-ingredientes-utilizados],
+          ...
+      }
+  }); */
 
 export default function Login() {
   const [validation, setValidation] = useState(true);
@@ -46,14 +74,16 @@ export default function Login() {
         onChange={(e) => setPassword(e.target.value)}
         required
       />
-      <button
-        data-testid="login-submit-btn"
-        type="button"
-        onClick={() => onclickFuntion()}
-        disabled={validation}
-      >
-        Entrar
-      </button>
+      <Link to="/comidas">
+        <button
+          data-testid="login-submit-btn"
+          type="button"
+          onClick={() => onclickFuntion(email)}
+          disabled={validation}
+        >
+          Entrar
+        </button>
+      </Link>
     </form>
   );
 }
