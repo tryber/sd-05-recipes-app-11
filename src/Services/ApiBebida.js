651572eee8c64f66b08668cc752818ee143f4a3a
ferrari <luisdiasferrari@gmail.com>
2020-09-10 22:49:01 -0300

    CC done

--- src/Services/ApiBebida.js
+++ src/Services/ApiBebida.js
@@ -1,27 +1,27 @@
 export const searchCockTailByName = (name) => {
-  fetchApi(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`).then((response) =>
+  fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`).then((response) =>
     response
       .json()
       .then((drink) => drink.drinks)
-      .catch((error) => error)
+      .catch((error) => error),
   );
 };
 
 export const searchCockTailByLetter = (letter) => {
-  fetchApi(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${letter}`).then((response) =>
+  fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${letter}`).then((response) =>
     response
       .json()
       .then((drink) => drink.drinks)
-      .catch((error) => error)
+      .catch((error) => error),
   );
 };
 
 export const searchCockTailByIngredient = (mainIngredient) => {
-  fetchApi(`https://www.thecocktaildb.com/api/json/v1/1/search.php?i=${mainIngredient}`).then(
+  fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?i=${mainIngredient}`).then(
     (response) =>
       response
         .json()
         .then((drink) => drink.drinks)
-        .catch((error) => error)
+        .catch((error) => error),
   );
 };
