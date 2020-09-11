690c495e49ce52bf1dc3fe1ac714d69260cae9d5
ferrari <luisdiasferrari@gmail.com>
2020-09-11 17:32:52 -0300

    teste Header

--- src/Components/Barra de Busca - Header/ButtonBuscar.jsx
+++ src/Components/Barra de Busca - Header/ButtonBuscar.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from 'react';
 
 export default function BottonBuscar() {
   return (
     <div data-testid="exec-search-btn">
       <button>Buscar</button>
     </div>
-  )
+  );
 }
