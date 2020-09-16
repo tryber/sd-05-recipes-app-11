a7ef21ee85c97dfb2602e598933a74dd640127ec
luca <luucadecastro@gmail.com>
2020-09-16 19:18:09 -0300

    tela explorar

--- src/Pages/TelaExplorar.jsx
+++ src/Pages/TelaExplorar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import MenuInferior from '../Components/MenuInferior';
 import BarraHeader from '../Components/Barra de Busca - Header/Header';
 
@@ -6,6 +7,15 @@ export default function TelaExplorar() {
   return (
     <div>
       <BarraHeader title={'Explorar'} showTop="true" />
+      <div>
+        <br/> <br/> <br/>
+        <Link to="/explorar/comidas" data-testid="explore-food" id="explorarComidas">
+          <button>Explorar Comidas</button>
+        </Link>
+        <Link to="/explorar/bebidas" data-testid="explore-drinks" id="explorarBebidas">
+          <button>Explorar Bebidas</button>
+        </Link>
+      </div>
       <MenuInferior />
     </div>
   );
