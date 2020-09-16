ed6d5aa86a2be57c3daccb533cbbe4e03b9741ae
ferrari <luisdiasferrari@gmail.com>
2020-09-16 16:26:19 -0300

    Processo

--- src/Pages/TelaReceitaProcesso.jsx
+++ src/Pages/TelaReceitaProcesso.jsx
@@ -2,5 +2,12 @@ import React from 'react';
 
 export default function TelaReceitaProcesso(props) {
   console.log(props);
-  return <div>.</div>;
+  return (
+    <div>
+      <img />
+      <h1>title</h1>
+      <h3>Categoia</h3>
+      <h3>Ingredientes</h3>
+    </div>
+  );
 }
