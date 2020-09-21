a3c4e301e8276708665fe3946ec088cbdaf41a2f
ferrari <luisdiasferrari@gmail.com>
2020-09-21 13:50:14 -0300

    tela de perfil

--- src/Pages/TelaPerfil.jsx
+++ src/Pages/TelaPerfil.jsx
@@ -6,6 +6,7 @@ import '../CSS/TelaPerfil.css';
 
 export default function TelaPerfil() {
   const email = JSON.parse(localStorage.getItem('user'));
+  
   return (
     <div>
       <BarraHeader title={'Perfil'} showTop="true" />
